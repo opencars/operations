f38b6e3075873737759e4ceca19754d36956ad67
Ali <github@shal.dev>
2021-03-17 18:25:52 +0200

    Fix empty list issue in gRPC server (#9)

--- cmd/grpc-server/main.go
+++ cmd/grpc-server/main.go
@@ -4,41 +4,46 @@ import (
 	"context"
 	"flag"
 	"os/signal"
+	"strconv"
 	"syscall"
 
 	_ "github.com/lib/pq"
 
-	"github.com/opencars/operations/pkg/api/http"
+	"github.com/opencars/operations/pkg/api/grpc"
 	"github.com/opencars/operations/pkg/config"
+	"github.com/opencars/operations/pkg/domain/user"
 	"github.com/opencars/operations/pkg/logger"
 	"github.com/opencars/operations/pkg/store/sqlstore"
 )
 
 func main() {
-	var configPath string
-
-	flag.StringVar(&configPath, "config", "./config/config.yaml", "Path to the configuration file")
+	cfg := flag.String("config", "config/config.yaml", "Path to the configuration file")
+	port := flag.Int("port", 3000, "Port of the server")
 
 	flag.Parse()
 
-	conf, err := config.New(configPath)
+	conf, err := config.New(*cfg)
 	if err != nil {
 		logger.Fatalf("config: %v", err)
 	}
+
+	logger.NewLogger(logger.LogLevel(conf.Log.Level), conf.Log.Mode == "dev")
 
 	store, err := sqlstore.New(&conf.DB)
 	if err != nil {
 		logger.Fatalf("store: %v", err)
 	}
 
-	logger.NewLogger(logger.LogLevel(conf.Log.Level), conf.Log.Mode == "dev")
+	svc := user.NewService(store.Operation())
+
+	addr := ":" + strconv.Itoa(*port)
+	api := grpc.New(addr, svc)
 
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
-	addr := ":8080"
 	logger.Infof("Listening on %s...", addr)
-	if err := http.Start(ctx, addr, &conf.Server, store.Operation()); err != nil {
-		logger.Fatalf("http server failed: %v", err)
+	if err := api.Run(ctx); err != nil {
+		logger.Fatalf("grpc: %v", err)
 	}
 }
