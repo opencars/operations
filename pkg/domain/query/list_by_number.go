c1d24b6bbd2d6435737c59281efd0575c0469428
Ali <github@shal.dev>
2022-01-16 15:46:56 +0200

    Fixes (#17)

--- pkg/domain/query/list_by_number.go
+++ pkg/domain/query/list_by_number.go
@@ -17,10 +17,11 @@ import (
 )
 
 type ListByNumber struct {
-	UserID string
-	Number string
-	Limit  string
-	Order  string
+	UserID  string
+	TokenID string
+	Number  string
+	Limit   string
+	Order   string
 }
 
 func (q *ListByNumber) Prepare() {
@@ -78,6 +79,7 @@ func (q *ListByNumber) Validate() error {
 func (q *ListByNumber) Event(operations ...model.Operation) schema.Producable {
 	msg := vehicle.OperationSearched{
 		UserId:       q.UserID,
+		TokenId:      q.TokenID,
 		Number:       q.Number,
 		ResultAmount: uint32(len(operations)),
 		SearchedAt:   timestamppb.New(time.Now().UTC()),
